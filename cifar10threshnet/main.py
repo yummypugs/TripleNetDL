38e1384db7a73d5465b0449caae0238585ac4d4f
Alexander <alexanderwuensche@hotmail.com>
2023-10-24 21:04:52 +0200

    quick fix

--- cifar10threshnet/main.py
+++ cifar10threshnet/main.py
@@ -64,6 +64,8 @@ class Solver(object):
 
         self.model = threshnet79().to(self.device)
         # self.model = threshnet95().to(self.device)
+        num_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad) ## This is our addition
+        print(f"The model has {num_params} trainable parameters.") ## This is our addition
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
         self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[75, 150], gamma=0.5)
