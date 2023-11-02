cbfced0efece73c408ebf8597bae87400e73344e
Alexander <alexanderwuensche@hotmail.com>
2023-11-02 14:22:06 +0100

    adadelta fix

--- cifar10triplenet/main_adadelta.py
+++ cifar10triplenet/main_adadelta.py
@@ -66,7 +66,8 @@ class Solver(object):
         num_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad) ## This is our addition
         print(f"The model has {num_params} trainable parameters.") ## This is our addition
 
-        self.optimizer = optim.Adadelta(self.model.parameters(), lr=self.lr)
+        self.optimizer = optim.Adadelta(self.model.parameters())
+        #self.model.parameters(), lr=self.lr
         self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[75, 150], gamma=0.5)
         self.criterion = nn.CrossEntropyLoss().to(self.device)
 
